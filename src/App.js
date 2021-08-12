a062f53ba849524d541821148b3c4ede305eb3ec
Nyuykisamo Lamfih <lamfihbilly@hotmail.com>
2021-08-12 11:07:43 +0100

    Header-complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-
+import Header from "./components/Header";
 function App() {
   return (
     <div className="app">
-      <h1>Lets build slack</h1>
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/" exact>
+              <Header />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
