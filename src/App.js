5715df86ea712069ad7ad65422cb15d149865473
Nyuykisamo Lamfih <lamfihbilly@hotmail.com>
2021-08-12 11:29:46 +0100

    sidebar-header-complete

--- src/App.js
+++ src/App.js
@@ -2,20 +2,31 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import styled from "styled-components";
+import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <div className="app">
       <Router>
-        <div>
-          <Switch>
-            <Route path="/" exact>
-              <Header />
-            </Route>
-          </Switch>
-        </div>
+        <>
+          <Header />
+          <AppBody>
+            <Sidebar />
+            <Switch>
+              <Route path="/" exact>
+                {/* chat */}
+              </Route>
+            </Switch>
+          </AppBody>
+        </>
       </Router>
     </div>
   );
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
